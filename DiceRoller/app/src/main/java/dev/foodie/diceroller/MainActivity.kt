6a79b114ab8fd87dcd8408ea08fdcdc2aaaddf97
Foodie <olaorefouad565@gmail.com>
2019-10-09 16:02:16 +0100

    started by building my first app in Kotlin: A Dice Roller, dived deeper into using namespaces, finding views effeectively, and the process of layout inflation and rendering. We also took a mini-introduction into the gradle scripts. app-level gradle scripts, and project-level. Looking gooood!!!

---
+++ DiceRoller/app/src/main/java/dev/foodie/diceroller/MainActivity.kt
@@ -0,0 +1,35 @@
+package dev.foodie.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    fun rollDice() {
+
+        val randomInt = Random().nextInt(6) + 1
+        val drawableRes = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+
+        diceImage.setImageResource(drawableRes)
+
+    }
+}
