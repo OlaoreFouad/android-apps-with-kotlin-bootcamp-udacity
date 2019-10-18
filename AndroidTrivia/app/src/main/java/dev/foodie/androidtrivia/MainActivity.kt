1f038d476b64b621e388d317d4380bd14ffae217
Foodie <olaorefouad565@gmail.com>
2019-10-18 07:37:34 +0100

    started learning about navigation within fragments and activities by using the Android Arch Component: Navigation. It is pretty slick, but also weird. It seems a bit complex. I might have to take it easy, or at least learn from various sources. All in all, it's really amazing

---
+++ AndroidTrivia/app/src/main/java/dev/foodie/androidtrivia/MainActivity.kt
@@ -0,0 +1,16 @@
+package dev.foodie.androidtrivia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import dev.foodie.androidtrivia.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+}
---
+++ AndroidTrivia/app/src/main/java/dev/foodie/androidtrivia/TitleFragment.kt
@@ -0,0 +1,25 @@
+package dev.foodie.androidtrivia
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import dev.foodie.androidtrivia.databinding.FragmentTitleBinding
+
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: FragmentTitleBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_title, container, false
+        )
+
+        return binding.root
+    }
+
+
+}
